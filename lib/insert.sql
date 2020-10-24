c80b5ef073b851800e64641d46e13ecdbbf72033
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-24 15:26:03 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,20 +1,3 @@
-INSERT INTO users (id, name, age) VALUES (1, "Robert", 24);
-INSERT INTO users (id, name, age) VALUES (2, "Paloma", 19);
-INSERT INTO users (id, name, age) VALUES (3, "Mike", 27);
-INSERT INTO users (id, name, age) VALUES (4, "Beth", 33);
-INSERT INTO users (id, name, age) VALUES (5, "Alex", 29);
-INSERT INTO users (id, name, age) VALUES (6, "Frank", 39);
-INSERT INTO users (id, name, age) VALUES (7, "Brad", 26);
-INSERT INTO users (id, name, age) VALUES (8, "Harry", 23);
-INSERT INTO users (id, name, age) VALUES (9, "Matt", 29);
-INSERT INTO users (id, name, age) VALUES (10, "Michelle", 36);
-INSERT INTO users (id, name, age) VALUES (11, "Kate", 29);
-INSERT INTO users (id, name, age) VALUES (12, "Moe", 39);
-INSERT INTO users (id, name, age) VALUES (13, "Brittany", 20);
-INSERT INTO users (id, name, age) VALUES (14, "Henry", 49);
-INSERT INTO users (id, name, age) VALUES (15, "Brenda", 27);
-INSERT INTO users (id, name, age) VALUES (16, "Margot", 43);
-INSERT INTO users (id, name, age) VALUES (17, "Chris", 29);
-INSERT INTO users (id, name, age) VALUES (18, "Marcia", 34);
-INSERT INTO users (id, name, age) VALUES (19, "Ralph", 59);
-INSERT INTO users (id, name, age) VALUES (20, "David", 63);
+INSERT INTO users (id, name, age) VALUES (1, "Robert", 24), (2, "Paloma", 19), (3, "Mike", 27), (4, "Beth", 33), (5, "Alex", 29), (6, "Frank", 39), (7, "Brad", 26), (8, "Harry", 23), (9, "Matt", 29), (10, "Michelle", 36), (11, "Kate", 29), (12, "Moe", 39), (13, "Brittany", 20), (14, "Henry", 49), (15, "Brenda", 27), (16, "Margot", 43), (17, "Chris", 29), (18, "Marcia", 34), (19, "Ralph", 59), (20, "David", 63);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Feed the children", );
+
